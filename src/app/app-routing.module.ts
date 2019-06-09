3009ef32e0c013ffe95839d5c0517e3dbb1bb805
Nathan Ribeiro <nathanpirral@gmail.com>
2019-06-09 19:53:35 -0300

    Commit Prod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { ImobleComponent } from './imoble/imoble.component';
 import { HotsiteComponent } from './hotsite/hotsite.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'imoble', pathMatch: 'full' },
-  {path: 'imoble', component: ImobleComponent},
-  {path: 'eldorado', component: HotsiteComponent},
-  {path: '**', redirectTo: 'eldorado', pathMatch: 'full'}
+  {path: '', redirectTo: 'imobles', pathMatch: 'full' },
+  {path: 'imobles', component: ImobleComponent},
+  {path: 'terramundieldorado', component: HotsiteComponent},
+  {path: '**', redirectTo: 'imobles', pathMatch: 'full'}
 ];
 
 @NgModule({
